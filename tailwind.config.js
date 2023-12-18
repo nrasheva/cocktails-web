f92a86a791c0db61a8720073f1c4dbcc780ceb25
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2023-12-18 17:24:29 +0200

    Add Tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   plugins: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orangada: '#fa4616',
+        border: '#efefef',
+      },
+      minHeight: {
+        maxScreen: '100svh',
+      },
+      fontFamily: {
+        staatliches: ['Staatliches', 'sans-serif'],
+      },
+      backgroundImage: {
+        home: "url('src/assets/home.jpg')",
+      },
+    },
   },
 };
