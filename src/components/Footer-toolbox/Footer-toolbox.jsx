ca647ece6016153f70379e9067d226a3a4609153
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2023-12-23 21:02:37 +0200

    Implementing Footer-toolbox

---
+++ src/components/Footer-toolbox/Footer-toolbox.jsx
@@ -0,0 +1,17 @@
+export const Toolbox = () => {
+  return (
+    <div className='flex justify-center bg-blueberry'>
+      <div className='flex text-white'>
+        <span className='lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+          <p>Find us</p>
+        </span>
+        <span className='lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+          <p>Sign up</p>
+        </span>
+        <span className='lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+          <p>Shop</p>
+        </span>
+      </div>
+    </div>
+  );
+};
