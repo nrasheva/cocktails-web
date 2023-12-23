109140315a2c21ed8a505bb83d66f51234694bca
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2023-12-23 21:09:55 +0200

    implementing icons to Footer-toolbox

--- src/components/Footer-toolbox/Footer-toolbox.jsx
+++ src/components/Footer-toolbox/Footer-toolbox.jsx
@@ -1,14 +1,20 @@
+import { faCartShopping, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export const Toolbox = () => {
   return (
     <div className='flex justify-center bg-blueberry'>
       <div className='flex text-white'>
-        <span className='lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+        <span className='flex items-center gap-2 lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+          <FontAwesomeIcon icon={faLocationDot} />
           <p>Find us</p>
         </span>
-        <span className='lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+        <span className='flex items-center gap-2 lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+          <FontAwesomeIcon icon={faEnvelope} />
           <p>Sign up</p>
         </span>
-        <span className='lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+        <span className='flex items-center gap-2 lg:px-36 px-8 py-4 cursor-pointer hover:bg-darkBerry'>
+          <FontAwesomeIcon icon={faCartShopping} />
           <p>Shop</p>
         </span>
       </div>
