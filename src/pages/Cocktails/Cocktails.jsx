dd817f47929000fe10b8adc5dfcd8e5dcf0d6a0a
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2023-12-26 12:07:12 +0200

    Implementing Cocktails page

---
+++ src/pages/Cocktails/Cocktails.jsx
@@ -0,0 +1,15 @@
+export const Cocktails = () => {
+  return (
+    <main className='bg-red-200 flex flex-col'>
+      <div className='bg-orangada flex flex-col px-80 py-10 justify-center text-white '>
+        <div className='flex flex-col gap-3'>
+          <h1 className='text-5xl font-oswald font-medium'>FIND YOUR PERFECT COINTREAU COCKTAIL</h1>
+          <p className='text-lg'>
+            Explore our variety of cocktail recipes. From timeless classics to original creations, learn how to make the
+            perfect drink.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
