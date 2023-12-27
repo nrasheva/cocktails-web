d62ed6a722058b90b0db65c0b1115315a6b177c4
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2023-12-27 14:40:46 +0200

    Implementing hover on image

--- src/components/Cocktail/Cocktail.jsx
+++ src/components/Cocktail/Cocktail.jsx
@@ -5,8 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const Cocktail = (props) => {
   return (
     <div className='cursor-pointer' onClick={props.onClick}>
-      <div className='border-b-1px border-navBorder'>
-        <img className='w-64 h-64' src={props.img} alt={props.name} />
+      <div className='border-b-1px border-navBorder overflow-hidden'>
+        <div className='group overflow-hidden'>
+          <img
+            className='w-64 h-64 inline-block align-middle transform group-hover:scale-105 transition duration-150'
+            src={props.img}
+            alt={props.name}
+          />
+        </div>
         <p className='font-oswald p-2 flex items-start'>{props.name}</p>
       </div>
       <div className='flex items-start text-xs p-2 gap-6'>
