2ed7665c98c03d2fc75e0fe898d5b55516bf575d
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2023-12-27 13:42:28 +0200

    Implementing icons to Cocktail component

--- src/components/Cocktail/Cocktail.jsx
+++ src/components/Cocktail/Cocktail.jsx
@@ -1,15 +1,21 @@
+import { faFaceGrinTongueSquint } from '@fortawesome/free-regular-svg-icons';
+import { faRankingStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export const Cocktail = (props) => {
   return (
-    <div className='bg-green-300 cursor-pointer'>
-      <div className='border-b-2 border-navBorder'>
+    <div className='cursor-pointer'>
+      <div className='border-b-1px border-navBorder'>
         <img className='w-64 h-64' src={props.img} alt={props.name} />
-        <p>{props.name}</p>
+        <p className='font-oswald p-2 flex items-start'>{props.name}</p>
       </div>
-      <div className='bg-purple-300 flex justify-around'>
-        <span>
+      <div className='flex items-start text-xs p-2 gap-6'>
+        <span className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faFaceGrinTongueSquint} />
           <p>{props.taste}</p>
         </span>
-        <span>
+        <span className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faRankingStar} />
           <p>{props.level}</p>
         </span>
       </div>
