5881bc94b084f8aa076866d50f034a27e3c19cc3
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-03 17:05:36 +0200

    Styling Recipe component

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,9 +1,23 @@
+import { faMartiniGlassCitrus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export const Recipe = (props) => {
   return (
-    <div>
-      <ul>
+    <div className='bg-purple-500 p-8'>
+      <div className='flex items-center gap-1'>
+        <FontAwesomeIcon icon={faMartiniGlassCitrus} style={{ color: '#fa4616' }} size='xl' />
+        <h2 className='text-blueberry font-semibold'>HOW TO MAKE</h2>
+      </div>
+      <ul className='xl:flex gap-4 py-8'>
         {props.recipe.map((step) => {
-          return <li key={step}>{step}</li>;
+          const [index, action] = step.split(':');
+
+          return (
+            <li key={step} className='flex flex-col items-start'>
+              <p className='text-orangada font-semibold'>{'Step ' + index}</p>
+              <p className='text-blueberry'>{action + '.'}</p>
+            </li>
+          );
         })}
       </ul>
     </div>
