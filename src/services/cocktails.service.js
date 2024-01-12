a8e9e546037ad9f8e6b5ffc5ae43a396aba5dfa8
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-12 21:17:02 +0200

    Implement getCocktail service

--- src/services/cocktails.service.js
+++ src/services/cocktails.service.js
@@ -4,3 +4,8 @@ export const getCocktails = async () => {
   const { data } = await axios.get(`/cocktails`);
   return { cocktails: data };
 };
+
+export const getCocktail = async (cocktailId) => {
+  const { data } = await axios.get(`/cocktails/details?id=${cocktailId}`);
+  return { cocktail: data };
+};
