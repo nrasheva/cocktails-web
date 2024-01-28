f3f25c75fdbe53aee268fde1c8eb049f1cb7ba29
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-28 12:08:07 +0200

    Implementing login service

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -1,5 +1,9 @@
 import { axios } from '../axios';
 
+export const login = async (email, password) => {
+  return await axios.post('/auth/login', { email, password });
+};
+
 export const register = async (email, password) => {
   return await axios.post('/auth/register', { email, password });
 };
