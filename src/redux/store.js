d7fb8ba7496ac98f5e105f1cd49c2a0c0056fa44
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-01-31 17:39:47 +0200

    Implementing authentication reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { authenticationReducer } from './reducers/authentication';
 import { cocktailsReducer } from './reducers/cocktails';
 
 export const store = configureStore({
   reducer: {
+    authentication: authenticationReducer,
     cocktails: cocktailsReducer,
   },
 });
