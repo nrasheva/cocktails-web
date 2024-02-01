41727425ae793f4fbead031796ad8901d197229c
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-02-01 13:31:39 +0200

    Update login service

--- src/services/authentication.service.js
+++ src/services/authentication.service.js
@@ -1,7 +1,8 @@
 import { axios } from '../axios';
 
 export const login = async (email, password) => {
-  return await axios.post('/auth/login', { email, password });
+  const { data } = await axios.post('/auth/login', { email, password });
+  return { token: data.access_token };
 };
 
 export const register = async (email, password) => {
