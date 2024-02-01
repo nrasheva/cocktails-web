25a34674662d900d87f7b67cc962b7561d650ed7
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-02-01 13:30:57 +0200

    Implementing instance interceptors

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,38 @@
 import axios from 'axios';
 
+import { router } from '../main';
+import { validateToken } from '../tools';
+
 const baseURL = 'http://localhost:3000';
 const timeout = 10000;
 
 axios.defaults.baseURL = baseURL;
 axios.defaults.timeout = timeout;
 
-export { axios };
+const instance = axios.create({ baseURL, timeout });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const controller = new AbortController();
+
+    const validToken = validateToken();
+
+    if (!validToken) {
+      controller.abort();
+      router.navigate('/login');
+    }
+
+    const token = localStorage.getItem('token');
+
+    config.headers['Authorization'] = `Bearer ${token}`;
+    return {
+      ...config,
+      signal: controller.signal,
+    };
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export { axios, instance };
