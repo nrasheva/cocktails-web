7dea825662ba70e5fafe055ccaaee55f94cba456
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-02-15 15:19:29 +0200

    Implementing password visibility to Register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,3 +1,5 @@
+import { faEye, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -13,6 +15,7 @@ export const Register = () => {
   const [error, setError] = useState('');
   const [warning, setWarning] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
 
@@ -45,21 +48,32 @@ export const Register = () => {
         <form
           onSubmit={(e) => e.preventDefault()}
           className='bg-lightOrangada rounded flex flex-col items-center gap-12 md:w-1/3 w-full md:my-8 px-8 md:py-12 py-40'>
-          <div className='flex flex-col gap-4 xl:w-4/5 w-full'>
-            <Input
-              type='email'
-              autoComplete='on'
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder='Email'
-              value={email}
-            />
-            <Input
-              type='password'
-              autoComplete='on'
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder='Password'
-              value={password}
-            />
+          <div className='xl:w-4/5 w-full'>
+            <div className='flex flex-col gap-4'>
+              <Input
+                type='email'
+                autoComplete='on'
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email'
+                value={email}
+              />
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                autoComplete='on'
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+                value={password}
+              />
+            </div>
+            <div className='relative'>
+              <span
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+                className='absolute bottom-2 right-0 flex items-center pr-3 cursor-pointer'>
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+              </span>
+            </div>
             {submitted && Boolean(warning.length) && <p className='text-blueberry font-bold'>{warning}</p>}
             {Boolean(error.length) && <p className='text-blueberry font-bold'>{error}</p>}
           </div>
