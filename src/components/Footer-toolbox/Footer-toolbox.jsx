69828ca658dc2c0d55db18ca05eeb64190854cf2
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-02-26 23:14:33 +0200

    Update footer-toolbox

--- src/components/Footer-toolbox/Footer-toolbox.jsx
+++ src/components/Footer-toolbox/Footer-toolbox.jsx
@@ -25,7 +25,9 @@ export const Toolbox = ({ onHideNavigation, onLogout }) => {
   return (
     <div className='flex justify-center bg-blueberry'>
       <div className='flex text-white gap-5'>
-        <span className='flex items-center gap-2 lg:px-36 sm:px-4 py-4 cursor-pointer hover:bg-darkBerry'>
+        <span
+          className='flex items-center gap-2 lg:px-36 sm:px-4 py-4 cursor-pointer hover:bg-darkBerry'
+          onClick={() => navigate('location')}>
           <FontAwesomeIcon icon={faLocationDot} />
           <p>Find us</p>
         </span>
