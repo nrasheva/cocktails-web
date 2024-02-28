57d26269af9df76fc09d0d64549c2eb9981cc0f5
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-02-28 23:24:35 +0200

    Implementing leaflet to Location

--- src/pages/Location/Location.jsx
+++ src/pages/Location/Location.jsx
@@ -1,10 +1,26 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 import { Intro } from '../../components/Intro/Intro';
 
 export const Location = () => {
+  const position = [42.6977, 23.3219];
+
   return (
-    <Intro
-      text='BAR LOCATOR'
-      description='Cocktailandia is a home for wonderful cocktails, find the closest place to try one of them!'
-    />
+    <>
+      <Intro
+        text='BAR LOCATOR'
+        description='Cocktailandia is a home for wonderful cocktails, find the closest place to try one of them!'
+      />
+      <MapContainer center={position} zoom={13} style={{ height: '500px', width: '100%' }}>
+        <TileLayer
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>COCKTAILANDIA</Popup>
+        </Marker>
+      </MapContainer>
+    </>
   );
 };
